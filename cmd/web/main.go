2a50252c801bd7efe2665e2b73c467d25bb76c0a
Ahmed Abdou <a.abd0o2390@gmail.com>
2026-04-06 02:33:57 +0200

    Update server port for deployment

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,24 +3,30 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"time"
+
 	"z01.ascii-art-web/pkg/middleware"
 	"z01.ascii-art-web/pkg/router"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	server := &http.Server{
-		Addr:         ":8080",
+		Addr:         ":" + port,
 		Handler:      middleware.Logging(router.Routes()),
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
 
-	log.Println("Server running at http://localhost:8080")
+	log.Println("Server running on port " + port)
 
 	err := server.ListenAndServe()
 	if err != nil && err != http.ErrServerClosed {
 		log.Fatalf("Server error: %v", err)
 	}
-
 }
